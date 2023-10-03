35f4c7fd9ad27fabb5799058835a324a47d0a498
gustom <127263147+gustomInc@users.noreply.github.com>
2023-10-02 21:26:41 -0600

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,34 @@
+# FUNCIONES
+x = 1
+
+# FUNCIONES CON PARAMETROS FINITOS Y KEYWORDS
+def saludar(nombre, edad):
+    print("Hola " + nombre + " EDAD: " + str(edad))
+
+saludar(edad = 20, nombre = "LEYDI")
+saludar(nombre = "LUIZ", edad = 20)
+saludar("VICTOR", 20)
+
+# FUNCIONES CON N PARAMETROS
+def asistencia(*alumnos):
+    for alumno in alumnos:
+        print("ASISTIO: " + alumno)
+
+asistencia("LAURA", "ANGEL", "OCTAVIO", "EMIR")
+asistencia("AXEL", "ROLEX")
+asistencia("CJ")
+
+name = input("Ingresa tu nombre completo\n")
+
+nameParts = name.split(" ")
+capitalizeNameParts = []
+
+for part in nameParts:
+    capitalizeNameParts.append(part.capitalize())
+
+capitalizeName = " ".join(capitalizeNameParts)
+
+print(capitalizeName)
+
+
+
